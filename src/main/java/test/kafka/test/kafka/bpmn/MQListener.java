cb7441d540e7f83f40b4b756d401c2a57c531ba7
odrling <florianbadie@odrling.xyz>
2022-05-06 19:11:57 +0200

    merge Incoming in a single class

--- src/main/java/test/kafka/test/kafka/bpmn/MQListener.java
+++ src/main/java/test/kafka/test/kafka/bpmn/MQListener.java
@@ -9,6 +9,7 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import avro.monitor.commands.Command;
 import avro.monitor.commands.ElementEvent;
+import avro.monitor.commands.SetXMICommand;
 import io.vertx.core.json.JsonObject;
 
 @ApplicationScoped
@@ -16,6 +17,25 @@ public class MQListener {
 
 	@Inject ModelRunner modelRunner;
 	@Inject TraceService traceService;
+
+	@Incoming("modelInputSetModel")
+	public void setModel(JsonObject setModelJsonObject) {
+		SetXMICommand setModelData = setModelJsonObject.mapTo(SetXMICommand.class);
+		System.out.println(setModelData);
+		try {
+			modelRunner.setXMI(setModelData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Command command = Command.newBuilder().setCommand(setModelData).build();
+		try {
+			traceService.save(command);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Incoming("modelInputEvent")
 	public void commandInput(JsonObject eventJsonObject) {
